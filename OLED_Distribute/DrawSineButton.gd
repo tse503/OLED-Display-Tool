38dd9fe97e3f32cc7df33e2ba5e31ab5eaecc08e
tse503 <59828419+tse503@users.noreply.github.com>
2022-05-14 19:32:05 +0100

    Added project files and copy of Godot executable

---
+++ OLED_Distribute/DrawSineButton.gd
@@ -0,0 +1,8 @@
+extends Button
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_global_position(Vector2(130, 330))
+	set_size(Vector2(100, 40))
+	set_text("Draw Sine (4 Hz)")
+	
